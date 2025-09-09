a3a07b93adea4ff3dc2daaf48b54ff6ac0b0acab
kettlycl <kettlycampos2gmail.com>
2025-09-09 17:34:48 -0400

    first commit

---
+++ dados/dados.js
@@ -0,0 +1,167 @@
+const colecaoJogos = [
+
+    {
+        id: 1,
+        genero: "Aventura",
+        plataforma: "PlayStation",
+        titulo: "Uncharted 4"
+    },
+    {
+        id: 2,
+        genero: "Corrida",
+        plataforma: "Xbox",
+        titulo: "Forza Horizon 5"
+    },
+    {
+        id: 3,
+        genero: "RPG",
+        plataforma: "PC",
+        titulo: "The Witcher 3"
+    },
+    {
+        id: 4,
+        genero: "Plataforma",
+        plataforma: "Nintendo Switch",
+        titulo: "Super Mario Odyssey"
+    },
+    {
+        id: 5,
+        genero: "Luta",
+        plataforma: "Arcade",
+        titulo: "Street Fighter II"
+    },
+    {
+        id: 6,
+        genero: "Estratégia",
+        plataforma: "PC",
+        titulo: "Age of Empires IV"
+    },
+    {
+        id: 7,
+        genero: "Simulação",
+        plataforma: "Mobile",
+        titulo: "The Sims Mobile"
+    },
+    {
+        id: 8,
+        genero: "FPS",
+        plataforma: "PlayStation",
+        titulo: "Call of Duty: Modern Warfare II"
+    },
+    {
+        id: 9,
+        genero: "Esporte",
+        plataforma: "Xbox",
+        titulo: "FIFA 24"
+    },
+    {
+        id: 10,
+        genero: "MMORPG",
+        plataforma: "PC",
+        titulo: "World of Warcraft"
+    },
+    {
+        id: 11,
+        genero: "Battle Royale",
+        plataforma: "PC",
+        titulo: "Fortnite"
+    },
+    {
+        id: 12,
+        genero: "Roguelike",
+        plataforma: "Switch",
+        titulo: "Hades"
+    },
+    {
+        id: 13,
+        genero: "Metroidvania",
+        plataforma: "PC",
+        titulo: "Hollow Knight"
+    },
+    {
+        id: 14,
+        genero: "Survival Horror",
+        plataforma: "PlayStation",
+        titulo: "Resident Evil 4 Remake"
+    },
+    {
+        id: 15,
+        genero: "MOBA",
+        plataforma: "PC",
+        titulo: "League of Legends"
+    },
+    {
+        id: 16,
+        genero: "Puzzle",
+        plataforma: "Mobile",
+        titulo: "Monument Valley"
+    },
+    {
+        id: 17,
+        genero: "Stealth",
+        plataforma: "PlayStation",
+        titulo: "The Last of Us Part II"
+    },
+    {
+        id: 18,
+        genero: "Mundo Aberto",
+        plataforma: "PC",
+        titulo: "Elden Ring"
+    },
+    {
+        id: 19,
+        genero: "Hack and Slash",
+        plataforma: "PlayStation",
+        titulo: "Devil May Cry 5"
+    },
+    {
+        id: 20,
+        genero: "Terror",
+        plataforma: "Xbox",
+        titulo: "The Medium"
+    },
+    {
+        id: 21,
+        genero: "RTS",
+        plataforma: "PC",
+        titulo: "StarCraft II"
+    },
+    {
+        id: 22,
+        genero: "Party Game",
+        plataforma: "Switch",
+        titulo: "Mario Party Superstars"
+    },
+    {
+        id: 23,
+        genero: "Visual Novel",
+        plataforma: "PC",
+        titulo: "Doki Doki Literature Club"
+    },
+    {
+        id: 24,
+        genero: "Indie",
+        plataforma: "PC",
+        titulo: "Celeste"
+    },
+    {
+        id: 25,
+        genero: "Sandbox",
+        plataforma: "PC",
+        titulo: "Minecraft"
+    },
+    {
+        id: 26,
+        genero: "Rhythm",
+        plataforma: "Mobile",
+        titulo: "Cytus"
+    },
+    {
+        id: 27,
+        genero: "Card Game",
+        plataforma: "PC",
+        titulo: "Gwent"
+    }
+]
+
+export default colecaoJogos;
